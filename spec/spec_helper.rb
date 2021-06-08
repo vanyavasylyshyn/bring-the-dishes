7298e8941d8bd959d1803e4c105671f24ed9e924
vanyavasylyshyn <ivasyly@softserveinc.com>
2021-06-08 13:46:08 +0300

    Added HTML formetter to somplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'simplecov'
 require 'simplecov-lcov'
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+]
 SimpleCov.start 'rails' do
   add_filter(%r{^/spec/})
   enable_coverage(:branch)
